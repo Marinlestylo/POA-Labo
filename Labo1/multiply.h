8c6a3c31602d3d0d202e135be37e45623209891d
Jonathan <chil.tfm@gmail.com>
2022-03-03 16:18:31 +0100

    feat: add class Operator, Add, Substract and Multiply

---
+++ Labo1/multiply.h
@@ -0,0 +1,17 @@
+//
+// Created by Jonathan on 03.03.2022.
+//
+
+#ifndef LABO1_MULTIPLY_H
+#define LABO1_MULTIPLY_H
+
+#include "operation.h"
+
+class Multiply : public Operation{
+public:
+    int apply(int a, int b) const;
+};
+
+
+
+#endif //LABO1_MULTIPLY_H
---
+++ Labo1/substract.h
@@ -0,0 +1,16 @@
+//
+// Created by Jonathan on 03.03.2022.
+//
+
+#ifndef LABO1_SUBSTRACT_H
+#define LABO1_SUBSTRACT_H
+
+#include "operation.h"
+
+class Substract : public Operation{
+public:
+    int apply(int a, int b) const;
+};
+
+
+#endif //LABO1_SUBSTRACT_H
