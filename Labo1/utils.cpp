2c65ed5336171e4dea98b2ffbb13af064c52263c
Marinlestylo <chil.tfm@gmail.com>
2022-03-16 11:36:26 +0100

    docs: added headers

--- Labo1/utils.cpp
+++ Labo1/utils.cpp
@@ -1,6 +1,12 @@
-//
-// Created by Jonathan on 16.03.2022.
-//
+/*
+-----------------------------------------------------------------------------------
+Nom du fichier  : add.h
+Auteur(s)       : Alexandre Jaquier, Jonathan Friedli
+Date creation   : 03.03.2022
+Description     : Implémentations des fonctions Utils.
+Compilateur     : Mingw-w64 g++ 8.1.0
+-----------------------------------------------------------------------------------
+*/
 
 #include "utils.h"
 #include <cstdlib>
--- Labo1/utils.h
+++ Labo1/utils.h
@@ -1,17 +1,28 @@
-//
-// Created by Jonathan on 16.03.2022.
-//
+/*
+-----------------------------------------------------------------------------------
+Nom du fichier  : utils.h
+Auteur(s)       : Alexandre Jaquier, Jonathan Friedli
+Date creation   : 03.03.2022
+Description     : Fichier contenant la déclaration de fonctions utils. Ce dernier
+                  défini des fonctions telles que floorMod (inspirée du
+                  java) et randomNumber permettant de générer un entier.
+Compilateur     : Mingw-w64 g++ 8.1.0
+-----------------------------------------------------------------------------------
+*/
 
 #ifndef LABO1_UTILS_H
 #define LABO1_UTILS_H
 
 /**
-* Génère un nombre aléatoire
-*/
+ * Génère un entier alétoire modulo la valeur passée en paramètre
+ * @param mod est le modulo à appliquer sur la valeur aléatoire.
+ * @return une valeure entre [0, mod - 1]
+ */
 unsigned randomNumber(unsigned mod);
 
 /**
- * Effectue un floormod entre 2 valeurs passées en paramètres
+ * Effectue un modulo mathématique entre 2 valeurs passées en paramètres. Le
+ * résultat ne peut pas être négatif.
  * @param a
  * @param b
  * @return le modulo des deux paramètres
