a558ff741490392a486819a01cac6b1e1710b67f
Jonathan <chil.tfm@gmail.com>
2022-03-17 14:34:25 +0100

    feat: project

---
+++ Lab02/Labo2/main.cpp
@@ -0,0 +1,16 @@
+/*
+-----------------------------------------------------------------------------------
+Nom du fichier  : main.cpp
+Auteur(s)       : Alexandre Jaquier, Jonathan Friedli
+Date creation   : 17.03.2022
+Description     : Fichier principal du programme permettant de tester le bon
+                  fonctionnement de la classe Squadron.
+Compilateur     : Mingw-w64 g++ 8.1.0
+-----------------------------------------------------------------------------------
+*/
+#include <iomanip>
+#include <iostream>
+
+int main() {
+    std::cout << "Hello wordl";
+}
