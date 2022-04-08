84a3b02db849060021bb9f31e7ccf8d6a09be61b
AJaquier <alex.jaquier@gmail.com>
2022-04-08 23:15:01 +0200

    feat: some tests

--- Lab02/Labo2/main.cpp
+++ Lab02/Labo2/main.cpp
@@ -16,29 +16,68 @@ Compilateur     : Mingw-w64 g++ 8.1.0
 #include "transporter.hpp"
 #include "squadron.hpp"
 
+void testShips(){
+    std::cout << "Creation et affichage de vaisseaux" << std::endl;
+    Shuttle shuttle(20);
+    Dreadnought dreadnought(40);
+    TieInterceptor tieInterceptor;
+    TieHunter tieHunter;
+    std::cout << shuttle << std::endl <<
+              dreadnought << std::endl <<
+              tieInterceptor<< std::endl<<
+              tieHunter << std::endl;
+    std::cout << "Changement de nom pour Dreadnought :" << std::endl;
+    dreadnought.setNickname("Dreadnought");
+    std::cout << dreadnought << std::endl;
+    std::cout << "Formule de consommation pour tieHunter :" << std::endl;
+    std::cout << tieHunter.getConsumption(100,100);
+}
+
+void test(){
+    std::cout << "Creation d'un Squadron" << std::endl;
+    Squadron squad1("test1");
+    std::cout << squad1 << std::endl;
+
+    std::cout << "Creation d'un Squadron constant" << std::endl;
+    const Squadron squadStat("teststatique");
+    std::cout << squadStat << std::endl;
+
+    std::cout << "Creation de vaisseaux" << std::endl;
+    Shuttle shuttle(20);
+    Dreadnought dreadnought(40);
+    TieInterceptor tieInterceptor;
+    TieHunter tieHunter;
+    std::cout << shuttle << std::endl <<
+              dreadnought << std::endl <<
+              tieInterceptor<< std::endl<<
+              tieHunter << std::endl;
+
+    std::cout << "Ajout des vaisseaux dans le squadron1" << std::endl;
+    squad1.addShipFromSquadron(shuttle);
+    squad1.addShipFromSquadron(dreadnought);
+    squad1.addShipFromSquadron(tieInterceptor);
+    squad1.addShipFromSquadron(tieHunter);
+    std::cout << squad1 << std::endl;
+
+    std::cout << "Test du constructeur de copie (squad2)" << std::endl;
+    Squadron squad2(squad1);
+    std::cout << squad2 << std::endl;
+    std::cout << "Test de la suppression des vaisseaux" << std::endl;
+
+
+    std::cout << "Choix du chef du squadron1 -> tieHunter" << std::endl;
+    squad1.setLeader(tieHunter);
+    std::cout << squad1 << std::endl;
+
+    std::cout << "Test de la formule de consommation " << std::endl;
+    squad1.getConsommation(9,0);
+    std::cout << squad1 << std::endl;
+
+
+    testShips();
+}
+
 int main() {
-	TieHunter a;
-	TieHunter b;
-
-	std::cout << a << std::endl;
-	std::cout << b << std::endl;
-	TieInterceptor c;
-	TieInterceptor d;
-	std::cout << c << std::endl;
-	std::cout << d << std::endl;
-
-
-	Shuttle black(9);
-	Shuttle black2(23.4);
-	std::cout << black << std::endl;
-	std::cout << black2 << std::endl;
-
-	Dreadnought black3(144448);
-	Dreadnought black4(23.4);
-	std::cout << black3 << std::endl;
-	std::cout << black4 << std::endl;
-	/*
-
     TieHunter blackLeader;
 	blackLeader.setNickname("Black leader");
 	TieHunter blackTwo;
@@ -49,9 +88,10 @@ int main() {
 	squad += blackTwo;
 	squad += shuttle;
     squad.setLeader(blackLeader);
-    Squadron kek(squad);
 	std::cout << squad << std::endl << std::endl;
 
-    std::cout << kek << std::endl;
-*/
+    //test();
+    return EXIT_SUCCESS;
 }
+
+
