41aad75eddb7e80ab8cf278ba6311f3e9f03ca98
Jonathan <chil.tfm@gmail.com>
2022-05-19 15:43:24 +0200

    feat: parsing command line arguments

--- Labo04/buffy/main.cpp
+++ Labo04/buffy/main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, Buffy!" << std::endl;
+int main(int argc, char *argv[]) {
+    int val[3];
+    if(argc != 4){
+        throw std::runtime_error("Usage : buffy <taille de la grille> <nombre d'humains> <nombre de zombies>");
+    }
+    for(unsigned i = 1; i < 4; ++i){
+        val[i-1] = std::stoi(argv[i]);
+        if(val[i-1] <= 0){
+            throw std::invalid_argument("La taille de la grille, le nombre d'humain et le nombre de"
+                                        " zombies doivent etre plus grand que 0");
+        }
+    }
     return 0;
 }
