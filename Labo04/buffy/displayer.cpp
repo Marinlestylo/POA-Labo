dcee31ac1293880eb80742e229ca1cf6979b8df7
Jonathan <chil.tfm@gmail.com>
2022-05-19 16:30:58 +0200

    feat: add displayer

---
+++ Labo04/buffy/displayer.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Jonathan on 19.05.2022.
+//
+
+#include <iostream>
+#include "displayer.hpp"
+
+Displayer::Displayer(unsigned int gridSize, std::list<Humanoid *> *humanoids) : gridSize(gridSize), humanoids(humanoids) {
+
+}
+
+void Displayer::displayAll() {
+    std::cout << "L'affichage n'est pas encore fait petit cochon !" << std::endl;
+}
